4bb4592d0b52ec55e869c5e7a2b6937c11c05534
CristianRdz <20213tn086@utez.edu.mx>
2024-03-08 14:32:51 -0600

    Se mejoro el readme.md y se añadio initconfig

--- Servicio/src/main/java/servicios/uno/config/AppConfig.java
+++ Servicio/src/main/java/servicios/uno/config/AppConfig.java
@@ -1,13 +1,42 @@
 package servicios.uno.config;
+
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import servicios.uno.model.Genero;
+import servicios.uno.model.Pelicula;
+import servicios.uno.repository.GeneroRepository;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import servicios.uno.repository.PeliculasRepository;
 
 @Configuration
 public class AppConfig {
 
+    @Autowired
+    private GeneroRepository generoRepository;
+
+    @Autowired
+    private PeliculasRepository peliculasRepository;
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @PostConstruct
+    public void init() {
+        //INSERT INTO peliculas.generos (nombre) VALUES ('Terror'); INSERT INTO peliculas.generos (nombre) VALUES ('Suspenso'); INSERT INTO peliculas.generos (nombre) VALUES ('Ficción'); INSERT INTO peliculas.generos (nombre) VALUES ('Documental'); INSERT INTO peliculas.generos (nombre) VALUES ('Drama'); INSERT INTO peliculas.generos (nombre) VALUES ('Comedia'); INSERT INTO peliculas.generos (nombre) VALUES ('Romance');
+        if (generoRepository.count() == 0) {
+            generoRepository.save(new Genero(null,"Terror"));
+            generoRepository.save(new Genero(null,"Suspenso"));
+            generoRepository.save(new Genero(null,"Ficción"));
+            generoRepository.save(new Genero(null,"Documental"));
+            generoRepository.save(new Genero(null,"Drama"));
+            generoRepository.save(new Genero(null,"Comedia"));
+            generoRepository.save(new Genero(null,"Romance"));
+        }
     }
 }
