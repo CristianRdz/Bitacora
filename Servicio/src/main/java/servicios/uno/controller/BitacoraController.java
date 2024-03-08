4e7a9a59a6122d7e0c28032c8671911506d3d271
CristianRdz <20213tn086@utez.edu.mx>
2024-03-08 11:25:00 -0600

    Se arreglaron cors y puerto de BD

--- Servicio/src/main/java/servicios/uno/controller/BitacoraController.java
+++ Servicio/src/main/java/servicios/uno/controller/BitacoraController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/bitacora")
+@CrossOrigin(origins = "http://localhost:5173")  // Especifica el origen permitido
 public class BitacoraController {
 
     @Autowired
